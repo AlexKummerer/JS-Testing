e12a72c5a8c38cd0193cf241cb31df6b149e2cf5
Kummey <alexander@thomas-kummerer.de>
2024-03-06 00:46:42 +0100

    adding e2e test

--- util.test.js
+++ util.test.js
@@ -1,4 +1,10 @@
-const { generateText, createElement, validateInput , checkAndGenerate } = require("./util.js");
+const puppeteer = require("puppeteer");
+const {
+  generateText,
+  createElement,
+  validateInput,
+  checkAndGenerate,
+} = require("./util.js");
 const { JSDOM } = require("jsdom");
 
 describe("util.js tests", () => {
@@ -16,8 +22,7 @@ describe("util.js tests", () => {
   });
 
   test("should generate a valid text output", () => {
-
-    expect( checkAndGenerate("John", 29)).toBe("John (29 years old)");
+    expect(checkAndGenerate("John", 29)).toBe("John (29 years old)");
   });
 
   test("validateInput correctly validates input", () => {
@@ -27,4 +32,24 @@ describe("util.js tests", () => {
     expect(validateInput("25", false, true)).toBe(true); // number as string, allowed
     expect(validateInput("John", false, true)).toBe(false); // non-number string, not allowed
   });
+
+  test("should create an element with text and class", async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+    //   slowMo: 80,
+    //   args: ["--window-size=1920,1080"],
+    });
+    const page = await browser.newPage();
+
+    await page.goto(
+      "file:///F:/FE/Javascript_Guide/testing-01-starting-setup/index.html"
+    );
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+    await page.click('input#age');
+    await page.type('input#age', '28');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Anna (28 years old)');
+  },10000);
 });
